7f864b22eeb7d598ddd76b00b845f3f7859cfa97
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-11 13:39:35 -0300

    Mapping Relationships
    Mapping Relationships

---
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -0,0 +1,10 @@
+package com.ead.authuser.repositories;
+
+import java.util.UUID;
+
+import com.ead.authuser.models.UserCourseModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUID>{
+}
