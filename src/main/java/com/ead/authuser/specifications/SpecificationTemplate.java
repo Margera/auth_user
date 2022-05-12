c96d290413f398ff41e4fd02fe45aea045aff325
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-11 22:12:07 -0300

    Update Specification
    Add specification for userCourse

--- src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,5 +1,10 @@
 package com.ead.authuser.specifications;
 
+import java.util.UUID;
+
+import javax.persistence.criteria.Join;
+
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -21,6 +26,13 @@ public class SpecificationTemplate {
         @Spec(path = "userStatus", spec = Equal.class)
     })
     public interface UserSpec extends Specification<UserModel>{
+    }
 
+    public static Specification<UserModel> userCourseId(final UUID courseId){
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userProd = root.join("userCourse");
+            return cb.equal(userProd.get("courseId"), courseId);
+        };
     }
 }
