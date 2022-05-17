990704cedb07ee7d38bc80fcca9031def7511144
Bruno Silva <bruno_bls23@hotmail.com>
2022-05-16 21:56:47 -0300

    Add UserCourse relatinships
    Add relationship users x Courses

---
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,43 @@
+package com.ead.authuser.clients;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.ead.authuser.dtos.CourseDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class UserClient {
+ 
+    @Autowired
+    RestTemplate restTemplate;
+
+    String REQUEST_URI = "http://localhost:8082";
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+        List<CourseDto> searchResult = null;
+        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
+            + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+        try {
+            
+            log.debug("Response Number of Elements: {} ", searchResult.size());
+        } catch (Exception e) {
+            log.debug("Erros request /courses {} ", e);
+        }    
+
+        log.info("Ending requests /courses userId {} ", userId);
+        return null;
+    }
+}
