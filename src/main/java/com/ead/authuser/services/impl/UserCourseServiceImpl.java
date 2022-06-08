878256d4da04034d494eeb4ca5f66969391fd53b
Bruno Silva <bruno_bls23@hotmail.com>
2022-06-08 13:45:53 -0300

    Corrections

--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -8,7 +8,9 @@ import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserCourseServiceImpl implements UserCourseService{
     
     @Autowired
