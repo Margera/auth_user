c79ac936717a7dcc6bc17bd56563e9a7022181ba
Bruno Silva <bruno_bls23@hotmail.com>
2022-09-12 21:04:24 -0300

    Sync PC

--- src/main/java/com/ead/authuser/configs/RabbitmqConfig.java
+++ src/main/java/com/ead/authuser/configs/RabbitmqConfig.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
